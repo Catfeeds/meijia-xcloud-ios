e84955469e1b6406f54746a4fad0d3a9bbaa7bfd
怕吗?不怕 <834519879@qq.com>
2016-09-13 16:43:27 +0800

    textView 调整

--- VideoInterfaceController/View/VideoArticleHeaderView.m
+++ VideoInterfaceController/View/VideoArticleHeaderView.m
@@ -18,6 +18,7 @@
     IBOutlet UILabel *totalPersonLabel;
     IBOutlet UILabel *priceLabel;
     IBOutlet UILabel *dis_priceLabel;
+    IBOutlet UITextView *textView;
 }
 
 @end
@@ -33,5 +34,7 @@
     totalPersonLabel.text = [NSString stringWithFormat:@"%@%@", model.total_view, @"人已看过"];
     priceLabel.text = [NSString stringWithFormat:@"￥%0.2f", model.price];
     dis_priceLabel.text = [NSString stringWithFormat:@"￥%0.2f", model. dis_price];
+//    [textView sizeToFit];
+    textView.text = @"据重庆大学建筑城规学院有关负责人介绍，“楼纳国际高校建造设计大赛”是“楼纳国际山地建筑艺术节”的首要组成部分，落址于中国贵州黔西南州楼纳国际建筑师公社。竞赛由贵州省黔西南州义龙试验区管理委员会主办，中国建筑中心(CBC)与贵州省楼纳建筑师公社文化发展有限公司承办，吸引了来自国内外17所高校的师生团队，包括清华大学、同济大学、东南大学、湖南大";
 }
 @end
